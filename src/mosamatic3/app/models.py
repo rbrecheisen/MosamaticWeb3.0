0ad286ee70a677de63905b905737bafffa612b37
Ralph Brecheisen <ralph.brecheisen@gmail.com>
2024-09-19 22:31:56 +0200

    Created data models

--- src/mosamatic3/app/models.py
+++ src/mosamatic3/app/models.py
@@ -1,3 +1,43 @@
-from django.db import models
+import os
+import uuid
 
-# Create your models here.
+from django.db import models
+from django.contrib.auth.models import User
+
+
+class DataSetModel(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False, unique=True)
+    name = models.CharField(max_length=1024, editable=True, null=False)
+    path = models.CharField(max_length=2048, editable=False, null=True, unique=True)
+    created = models.DateTimeField(auto_now_add=True)
+    owner = models.ForeignKey(
+        User, editable=False, related_name='+', on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
+
+
+class FileSetModel(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False, unique=True)
+    name = models.CharField(max_length=1024, editable=True, null=False)
+    path = models.CharField(max_length=2048, editable=False, null=True, unique=True)
+    created = models.DateTimeField(auto_now_add=True)
+    owner = models.ForeignKey(
+        User, editable=False, related_name='+', on_delete=models.CASCADE)
+    dataset = models.ForeignKey(DataSetModel, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
+
+
+class FileModel(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False, unique=True)
+    name = models.CharField(max_length=256, editable=False, null=False)
+    path = models.CharField(max_length=2048, editable=False, null=False, unique=True)
+    created = models.DateTimeField(auto_now_add=True)
+    owner = models.ForeignKey(
+        User, editable=False, related_name='+', on_delete=models.CASCADE)
+    fileset = models.ForeignKey(FileSetModel, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return os.path.split(str(self.path))[1]
