87a8552a4a901dd8a18d39049eb4bc8fcf4b56d1
Ralph Brecheisen <ralph.brecheisen@gmail.com>
2024-09-20 16:39:37 +0200

    Saving work

--- src/mosamatic3/app/views.py
+++ src/mosamatic3/app/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
+from django.contrib.auth.decorators import login_required
+from django.http import HttpResponse, HttpResponseForbidden
+from wsgiref.util import FileWrapper
 
-# Create your views here.
+
+@login_required
+def auth(_):
+    return HttpResponse(status=200)
+
+
+@login_required
+def datasets(request):
+    return render(request, 'datasets.html', context={'datasets': []})
