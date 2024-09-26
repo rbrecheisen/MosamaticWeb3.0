c6ba8e00ea6f6b197cea94bcff2d067829328acd
Ralph Brecheisen <ralph.brecheisen@gmail.com>
2024-09-26 21:46:31 +0200

    Created task pages

--- src/mosamatic3/app/urls.py
+++ src/mosamatic3/app/urls.py
@@ -3,6 +3,10 @@ from django.urls import path
 from .views.base import auth, custom_logout
 from .views.filesets import fileset, filesets
 from .views.tasks.tasks import tasks
+from .views.tasks.filterdicom import filterdicom
+from .views.tasks.transformdicom import transformdicom
+from .views.tasks.musclefatsegmentation import musclefatsegmentation
+from .views.tasks.calculatebodycompositionmetrics import calculatebodycompositionmetrics
 
 
 urlpatterns = [
@@ -12,4 +16,8 @@ urlpatterns = [
     path('filesets/<str:fileset_id>', fileset),
     path('accounts/logout/', custom_logout, name='logout'),
     path('tasks/', tasks),
+    path('tasks/filterdicom/', filterdicom),
+    path('tasks/transformdicom/', transformdicom),
+    path('tasks/musclefatsegmentation/', musclefatsegmentation),
+    path('tasks/calculatebodycompositionmetrics/', calculatebodycompositionmetrics),
 ]
