9cfec0dc4a5159b1f3fbcaf2a3befb384d983550
Ralph Brecheisen <r.brecheisen@maastrichtuniversity.nl>
2024-10-08 20:51:30 +0200

    App works on arm64 (M1 macbook)

--- runscripts/Shutdown.bat
+++ runscripts/Shutdown.bat
@@ -1,2 +1,3 @@
-docker-compose down --volumes
-docker volume prune -f
+@REM docker-compose down --volumes
+@REM docker volume prune -f
+docker-compose down
--- runscripts/Shutdown.sh
+++ runscripts/Shutdown.sh
@@ -1,2 +1,3 @@
-docker-compose down --volumes
-docker volume prune -f
+# docker-compose down --volumes
+# docker volume prune -f
+docker-compose down
