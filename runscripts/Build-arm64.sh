475c9959101326ede39bacadfd39af527f710fc5
Brecheisen <r.brecheisen@maastrichtuniversity.nl>
2024-10-15 09:29:35 +0200

    Added system prune to build scripts

--- runscripts/Build-arm64.sh
+++ runscripts/Build-arm64.sh
@@ -1,2 +1,3 @@
 runscripts/Shutdown.sh
 docker-compose -f docker-compose-arm64-build.yml build --no-cache
+docker system prune -f
--- runscripts/Build.sh
+++ runscripts/Build.sh
@@ -1,2 +1,3 @@
 runscripts/Shutdown.sh
 docker-compose -f docker-compose-build.yml build
+docker system prune -f
