9c4861c3b84ed69c7c6925a9e5e4717f4286e90f
Brecheisen <r.brecheisen@maastrichtuniversity.nl>
2024-10-21 10:06:05 +0200

    Added deploy scripts to intel and arm64 dirs

---
+++ intel/deploy.bat
@@ -0,0 +1,7 @@
+@echo off
+set /p PASSWORD=<C:\\Users\\r.brecheisen\\dockerhub.txt
+docker logout
+docker login --username brecheisen --password "%PASSWORD%"
+docker push brecheisen/mosamatic3-nginx-amd64:latest
+docker push brecheisen/mosamatic3-huey-amd64:latest
+docker push brecheisen/mosamatic3-web-amd64:latest
