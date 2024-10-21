ede1a1ca32781a2a4e8727d82bf9dfefcc34255c
Brecheisen <r.brecheisen@maastrichtuniversity.nl>
2024-10-21 10:50:05 +0200

    Renamed FilterDicom to CheckDicom

--- src/mosamatic3/app/tasks/checkdicomtask.py
+++ src/mosamatic3/app/tasks/checkdicomtask.py
@@ -11,55 +11,46 @@ from .taskexception import TaskException
 LOG = LogManager()
 
 
-def process_file(f, rows: int, cols: int, rows_equals: bool, cols_equals: bool) -> bool:
+def process_file(f) -> bool:
     try:
         p = pydicom.dcmread(f.path, stop_before_pixels=True)
-        LOG.info(f'rows: {rows}, cols: {cols}, p.Rows: {p.Rows}, p.Columns: {p.Columns}')
+        LOG.info(f'file: {f.path}, p.Rows: {p.Rows}, p.Columns: {p.Columns}')
         ok = True
-        if rows_equals:
-            if p.Rows != rows:
-                ok = False
-        else:
-            if p.Rows == rows:
-                ok = False
-        if cols_equals:
-            if p.Columns != cols:
-                ok = False
-        else:
-            if p.Columns == cols:
-                ok = False
+        if p.Rows != 512 or p.Columns != 512:
+            LOG.info(f'Image {f.path} has dimensions {p.Rows} x {p.Columns} (should be 512 x 512)')
+            ok = False
         return ok
     except pydicom.errors.InvalidDicomError:
         return False
 
 
 @task()
-def filterdicomtask(task_status_id: str, fileset_id: str, output_fileset_name: str, user :User, rows: int, cols: int, rows_equals: bool, cols_equals: bool) -> bool:
-    name = 'filterdicomtask'
-    LOG.info(f'name: {name}, task_status_id: {task_status_id}, fileset_id: {fileset_id}, output_fileset_name: {output_fileset_name}, rows: {rows}, cols: {cols}, rows_equals: {rows_equals}, cols_equals: {cols_equals}')
+def checkdicomtask(task_status_id: str, fileset_id: str, output_fileset_name: str, user :User) -> bool:
+    name = 'checkdicomtask'
+    LOG.info(f'name: {name}, task_status_id: {task_status_id}, fileset_id: {fileset_id}, output_fileset_name: {output_fileset_name}')
     data_manager = DataManager()
     if not is_uuid(fileset_id):
-        raise TaskException('filterdicomtask() fileset_id is not UUID')
+        raise TaskException(f'{name}() fileset_id is not UUID')
     fileset = data_manager.get_fileset(fileset_id)
     files = data_manager.get_files(fileset)
-    new_files = []
+    new_files, wrong_files = [], []
     nr_steps = len(files)
     set_task_status(name, task_status_id, {'status': 'running', 'progress': 0})
     for step in range(nr_steps):
-        LOG.info(f'filterdicomtask() processing file {files[step].path}...')
-        if not process_file(files[step], rows, cols, rows_equals, cols_equals):
-            continue
+        LOG.info(f'{name}() processing file {files[step].path}...')
+        if not process_file(files[step]):
+            wrong_files.append(files[step])
         else:
             new_files.append(files[step])        
         progress = int(((step + 1) / (nr_steps)) * 100)
         set_task_status(name, task_status_id, {'status': 'running', 'progress': progress})
-        import time
-        time.sleep(1)
     if len(new_files) > 0:
         new_fileset = data_manager.create_fileset(user, output_fileset_name)
         for f in new_files:
             data_manager.create_file(f.path, new_fileset)
-    else:
-        LOG.warning(f'filterdicomtask() new fileset is empty')
+    if len(wrong_files) > 0:
+        wrong_fileset = data_manager.create_fileset(user, output_fileset_name + '-not512x512')
+        for f in wrong_files:
+            data_manager.create_file(f.path, wrong_fileset)
     set_task_status(name, task_status_id, {'status': 'completed', 'progress': 100})
     return True
---
+++ src/mosamatic3/app/views/tasks/checkdicom.py
@@ -0,0 +1,31 @@
+from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
+
+from ...tasks.checkdicomtask import checkdicomtask
+from ...tasks.taskmanager import TaskManager
+from ...data.datamanager import DataManager
+from ...data.logmanager import LogManager
+
+LOG = LogManager()
+
+
+@login_required
+def checkdicom(request):
+    data_manager = DataManager()
+    task_manager = TaskManager()
+    if request.method == 'POST':
+        fileset_id = request.POST.get('fileset_id', None)
+        if fileset_id:
+            output_fileset_name = request.POST.get('output_fileset_name', None)
+            return task_manager.run_task_and_get_response(checkdicomtask, fileset_id, output_fileset_name, request.user)
+        else:
+            LOG.warning(f'views.tasks.checkdicom: no fileset ID in POST request')
+            pass
+    elif request.method == 'GET':
+        response = task_manager.get_response('checkdicomtask', request)
+        if response:
+            return response
+    else:
+        pass
+    filesets = data_manager.get_filesets(request.user)
+    return render(request, 'tasks/checkdicom.html', context={'filesets': filesets})
