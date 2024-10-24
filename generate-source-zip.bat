d36905d8ec9a4d3f24d5ca2dfb96aa71f40ba1eb
Ralph Brecheisen <ralph.brecheisen@gmail.com>
2024-10-24 11:19:51 +0200

    Savnig work

--- generate-source-zip.bat
+++ generate-source-zip.bat
@@ -1,5 +1,7 @@
 @echo off
-set ZIP_NAME="C:\Users\r.brecheisen\Desktop\MosamaticWeb3.0-source.zip"
+
+for /f "tokens=*" %%i in ('git rev-parse --short HEAD') do set GIT_COMMIT_ID=%%i
+set ZIP_NAME="C:\Users\r.brecheisen\Desktop\MosamaticWeb3.0-source-%GIT_COMMIT_ID%.zip"
 set FILE_LIST=sources.txt
 PowerShell -Command "Compress-Archive -Path (Get-Content %FILE_LIST%) -DestinationPath %ZIP_NAME% -Force"
 pause
