3e57e10ef4b960d324dd16a0d223ef8f1276dfd8
Ralph Brecheisen <ralph.brecheisen@gmail.com>
2024-10-24 12:38:42 +0200

    Generate source code ZIP updates shell line endings

--- generate-source-zip.bat
+++ generate-source-zip.bat
@@ -3,5 +3,17 @@
 for /f "tokens=*" %%i in ('git rev-parse --short HEAD') do set GIT_COMMIT_ID=%%i
 set ZIP_NAME="C:\Users\r.brecheisen\Desktop\MosamaticWeb3.0-source-%GIT_COMMIT_ID%.zip"
 set FILE_LIST=sources.txt
-PowerShell -Command "Compress-Archive -Path (Get-Content %FILE_LIST%) -DestinationPath %ZIP_NAME% -Force"
+@REM PowerShell -Command "Compress-Archive -Path (Get-Content %FILE_LIST%) -DestinationPath %ZIP_NAME% -Force"
+@REM PowerShell -NoProfile -ExecutionPolicy Bypass -Command ^
+@REM     "foreach ($item in Get-Content '%TEMP_FILE%') { Compress-Archive -Path $item.Trim() -DestinationPath '%ZIP_NAME%' -Update }"
+
+for %%F in ("%FILE_LIST%") do (
+    if /I "%%~xF"==".sh" (
+        powershell -Command "(Get-Content %%F) | Set-Content -NoNewline -Encoding ASCII %%F"
+    )
+)
+
+PowerShell -NoProfile -ExecutionPolicy Bypass -Command ^
+    "foreach ($item in Get-Content '%FILE_LIST%' -Raw -Delimiter `n) { Compress-Archive -Path $item.Trim() -DestinationPath '%ZIP_NAME%' -Update }"
+    
 pause
