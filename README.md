7d64d4eecdaf5f85fcd855bf8c65a0076371c9b2
Brecheisen <r.brecheisen@maastrichtuniversity.nl>
2024-11-15 15:50:48 +0100

    Updated comments and README

--- README.md
+++ README.md
@@ -1,1 +1,19 @@
-# MosamaticWeb3.0
+# Mosamatic Web 3.0
+A web-based tool for analyzing medical images and calculating body composition (muscle and fat) information.
+
+# Versions
+- v3.0.0
+    - Automatic selection of L3 from full CT scan
+    - L3-level muscle and fat segmentation
+    - Calculation of body composition metrics
+    - XNAT full CT scan import
+    - TensorFlow AI
+- v3.1.0
+    - Automatic selection of arbitrary slices from full CT scan
+    - T4-level muscle and fat segmentation
+    - PyTorch AI
+- v3.2.0
+    - Automatic selection of all spinal slices from full CT scan
+    - Full-spine muscle and fat segmentation (semi 3D)
+    - Volume measurements for muscle and fat (along spinal column)
+    - Mean, median and standard deviation of mean HU (radiation attenuation) along spinal column for muscle, SAT and VAT
--- src/mosamatic3/app/tasks/task.py
+++ src/mosamatic3/app/tasks/task.py
@@ -11,6 +11,36 @@ LOG = LogManager()
 
 class Task:
     def __init__(self, name: str, display_name: str, description: str, html_page: str, url_pattern: str, task_func, parameter_names: List[str], visible: bool=True) -> None:
+        """
+        The parent Task constructor requires the following arguments:
+
+        - name              : Fully lower-case name of task (without spaces, e.g., "checkdicomtask"). This name will be used
+                              to keep track of the task's status in Redis. For consistency, make this name end in "task"
+        - display_name      : Task name to display in the HTML page
+        - description       : A one-line description of what the task accomplishes, also displayed in the HTML page
+        - html_page         : Name of the HTML page file for this task. It should be "tasks/<task name>.html" where <task name> is equal to task's "name" parameter
+        - url_pattern       : The URL pattern for this task's view. Should start with 'tasks/<view name>', so no leading forward slash. The <view name>
+                              should be same as the task's "name" parameter.
+        - task_func         : The task function object that gets called by the Huey backend when the task needs to execute
+        - parameter_names   : The names of any HTML POST parameters that will be passed to the task's view when the form in the HTML page gets submitted.
+                              The view will automatically extract these parameters (by their name) and pass them to the task's execution function
+        - visible           : Whether this task should be listed in the tasks.html page
+
+        Example task with __init__() constructor:
+
+        class BodyCompositionMetricsTask(Task):
+            def __init__(self) -> None:
+                super(BodyCompositionMetricsTask, self).__init__(
+                    name='bodycompositionmetricstask',
+                    display_name='Body composition metrics',
+                    description='This task calculates body composition metrics from L3 images and corresponding muscle and fat segmentations',
+                    html_page='tasks/bodycompositionmetricstask.html',
+                    url_pattern='/tasks/bodycompositionmetricstask',
+                    task_func=bodycompositionmetricstask,
+                    parameter_names=['fileset_id', 'segmentation_fileset_id', 'output_fileset_name', 'patient_heights_fileset_id'],
+                    visible=True,
+                )
+        """
         self.name = name
         self.display_name = display_name
         self.description = description
@@ -28,8 +58,8 @@ class Task:
         task_manager = TaskManager()
         task_model = data_manager.get_task_by_name(self.name)
         if request.method == 'POST':
-            task_parameters = {'user': request.user}
-            for parameter_name in self.parameter_names:
+            task_parameters = {'user': request.user} # Always add "user" parameter
+            for parameter_name in self.parameter_names: # Iterate through task's HTML POST parameters as specified
                 task_parameters[parameter_name] = request.POST.get(parameter_name, None)
             return task_manager.run_task_and_get_response(self.task_func, task_parameters)
         elif request.method == 'GET':
