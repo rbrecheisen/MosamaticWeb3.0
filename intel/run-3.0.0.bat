2535261a9d84b2144780d354ba687b50dfc52400
Brecheisen <r.brecheisen@maastrichtuniversity.nl>
2024-11-18 14:24:02 +0100

    Changed repo to public and updated run script

--- intel/run-3.0.0.bat
+++ intel/run-3.0.0.bat
@@ -2,16 +2,18 @@
 setlocal enabledelayedexpansion
 
 @rem Check if shutdown.bat available. If so, call it
-if exist "shutdown-3.0.0.bat" (
-    call shutdown-3.0.0.bat
+if not exist "shutdown-3.0.0.bat" (
+    echo "Downloading shutdown.bat..."
+    powershell -Command "Invoke-WebRequest -Uri 'https://raw.githubusercontent.com/rbrecheisen/MosamaticWeb3.0/refs/heads/main/intel/shutdown-3.0.0.bat' -OutFile 'shutdown-3.0.0.bat'"
 )
 
 @rem Check if docker-compose.yml available. If not, download it
 if not exist "docker-compose-3.0.0.yml" (
     echo "Downloading docker-compose.yml..."
-    powershell -Command "Invoke-WebRequest -Uri 'https://raw.githubusercontent.com/rbrecheisen/MosamaticWeb3.0/refs/heads/main/intel/docker-compose-3.0.0.yml?token=GHSAT0AAAAAACXVUKOOL7RATQ7XBG7S7UB4ZZ3H7LA' -OutFile 'docker-compose-3.0.0.yml'"
+    powershell -Command "Invoke-WebRequest -Uri 'https://raw.githubusercontent.com/rbrecheisen/MosamaticWeb3.0/refs/heads/main/intel/docker-compose-3.0.0.yml' -OutFile 'docker-compose-3.0.0.yml'"
 )
 
 @rem Run application
+call shutdown-3.0.0.bat
 docker-compose -f docker-compose-3.0.0.yml up -d 
 docker-compose -f docker-compose-3.0.0.yml logs -f web huey
