ccf0379bef3042371490eea8c0e2e804e7bdafb5
Ralph Brecheisen <r.brecheisen@maastrichtuniversity.nl>
2024-11-19 13:42:00 +0100

    Saving work

--- versions/arm64/3.0.0/cpu/run.sh
+++ versions/arm64/3.0.0/cpu/run.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
 if [[ ! -f "shutdown.bat" ]]; then
-    echo "Downloading shutdown.bat..."
-    curl -o "shutdown.bat" "https://raw.githubusercontent.com/rbrecheisen/MosamaticWeb3.0/refs/heads/main/versions/arm64/3.0.0/cpu/shutdown.bat"
+    echo "Downloading shutdown.sh..."
+    curl -o "shutdown.sh" "https://raw.githubusercontent.com/rbrecheisen/MosamaticWeb3.0/refs/heads/main/versions/arm64/3.0.0/cpu/shutdown.sh"
     chmod +x ./shutdown.sh
 fi
 
 if [[ ! -f "docker-compose.yml" ]]; then
     echo "Downloading docker-compose.yml..."
-    curl -o "shutdown.bat" "https://raw.githubusercontent.com/rbrecheisen/MosamaticWeb3.0/refs/heads/main/versions/arm64/3.0.0/cpu/docker-compose.yml"
+    curl -o "docker-compose.yml" "https://raw.githubusercontent.com/rbrecheisen/MosamaticWeb3.0/refs/heads/main/versions/arm64/3.0.0/cpu/docker-compose.yml"
 fi
 
 ./shutdown.sh
