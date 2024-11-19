4b573db57eddb32726310b2b3f9a57aa37a20100
Ralph Brecheisen <r.brecheisen@maastrichtuniversity.nl>
2024-11-19 13:37:09 +0100

    Updated run script MacOS arm64

--- versions/arm64/3.0.0/cpu/run.sh
+++ versions/arm64/3.0.0/cpu/run.sh
@@ -1,3 +1,15 @@
 #!/bin/bash
+
+if [[ ! -f "shutdown.bat" ]]; then
+    echo "Downloading shutdown.bat..."
+    curl -o "shutdown.bat" "https://raw.githubusercontent.com/rbrecheisen/MosamaticWeb3.0/refs/heads/main/versions/arm64/3.0.0/cpu/shutdown.bat"
+    chmod +x ./shutdown.sh
+fi
+
+if [[ ! -f "docker-compose.yml" ]]; then
+    echo "Downloading docker-compose.yml..."
+    curl -o "shutdown.bat" "https://raw.githubusercontent.com/rbrecheisen/MosamaticWeb3.0/refs/heads/main/versions/arm64/3.0.0/cpu/docker-compose.yml"
+fi
+
 ./shutdown.sh
 docker-compose up -d && docker-compose logs -f web huey
