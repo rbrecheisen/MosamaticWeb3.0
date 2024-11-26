8ae8483f672a20911a7a38cc8dc511a68175c245
Ralph Brecheisen <r.brecheisen@maastrichtuniversity.nl>
2024-11-26 11:50:23 +0100

    Created run.sh script

---
+++ versions/intel/3.0.0/gpu/run.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+if [ ! -e "shutdown.sh" ]; then
+    echo "Downloading shutdown.sh..."
+    curl -O "https://raw.githubusercontent.com/rbrecheisen/MosamaticWeb3.0/refs/heads/main/versions/intel/3.0.0/gpu/shutdown.sh"
+fi
+
+if [ ! -e "docker-compose.yml" ]; then
+    echo "Downloading docker-compose.yml..."
+    curl -O "https://raw.githubusercontent.com/rbrecheisen/MosamaticWeb3.0/refs/heads/main/versions/intel/3.0.0/gpu/docker-compose.yml"
+fi
+
+./shutdown.sh
+docker-compose up -d && docker-compose logs -f web huey
