7e834d6d249fe00365ed518e96191107d4343821
Brecheisen <r.brecheisen@maastrichtuniversity.nl>
2024-11-26 13:24:47 +0100

    Saving work

--- versions/intel/3.0.0/cpu/run.sh
+++ versions/intel/3.0.0/cpu/run.sh
@@ -2,13 +2,13 @@
 
 if [ ! -e "shutdown.sh" ]; then
     echo "Downloading shutdown.sh..."
-    curl -O "https://raw.githubusercontent.com/rbrecheisen/MosamaticWeb3.0/refs/heads/main/versions/intel/3.0.0/cpu/shutdown.sh"
+    curl -o "shutdown.sh" "https://raw.githubusercontent.com/rbrecheisen/MosamaticWeb3.0/refs/heads/main/versions/intel/3.0.0/cpu/shutdown.sh"
     chmod +x ./shutdown.sh
 fi
 
 if [ ! -e "docker-compose.yml" ]; then
     echo "Downloading docker-compose.yml..."
-    curl -O "https://raw.githubusercontent.com/rbrecheisen/MosamaticWeb3.0/refs/heads/main/versions/intel/3.0.0/cpu/docker-compose-prod.yml"
+    curl -o "docker-compose.yml" "https://raw.githubusercontent.com/rbrecheisen/MosamaticWeb3.0/refs/heads/main/versions/intel/3.0.0/cpu/docker-compose-prod.yml"
 fi
 
 ./shutdown.sh
--- versions/intel/latest/gpu/run.sh
+++ versions/intel/latest/gpu/run.sh
@@ -2,13 +2,13 @@
 
 if [ ! -e "shutdown.sh" ]; then
     echo "Downloading shutdown.sh..."
-    curl -O "https://raw.githubusercontent.com/rbrecheisen/MosamaticWeb3.0/refs/heads/main/versions/intel/latest/gpu/shutdown.sh"
+    curl -o "shutdown.sh" "https://raw.githubusercontent.com/rbrecheisen/MosamaticWeb3.0/refs/heads/main/versions/intel/latest/gpu/shutdown.sh"
     chmod +x ./shutdown.sh
 fi
 
 if [ ! -e "docker-compose.yml" ]; then
     echo "Downloading docker-compose.yml..."
-    curl -O "https://raw.githubusercontent.com/rbrecheisen/MosamaticWeb3.0/refs/heads/main/versions/intel/latest/gpu/docker-compose-prod.yml"
+    curl -o "docker-compose.yml" "https://raw.githubusercontent.com/rbrecheisen/MosamaticWeb3.0/refs/heads/main/versions/intel/latest/gpu/docker-compose-prod.yml"
 fi
 
 ./shutdown.sh
