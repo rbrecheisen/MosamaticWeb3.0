534cb61b191383ee63f922e740d924d543c3c094
Brecheisen <r.brecheisen@maastrichtuniversity.nl>
2025-01-29 10:01:55 +0100

    Finished data preparation script Maastro T4

--- experiments/organizet4datamaastro/main.py
+++ experiments/organizet4datamaastro/main.py
@@ -11,3 +11,23 @@ steps = {
 
 
 if steps["copy"]:
+    for d in os.listdir(data_dir_org):
+        d_path = os.path.join(data_dir_org, d)
+        if os.path.isdir(d_path):
+            for f in os.listdir(d_path):
+                if f.endswith('.dcm'):
+                    elems = os.path.splitext(f)
+                    f_name = elems[0]
+                    f_path = os.path.join(d_path, f)
+                    f_path_new = os.path.join(data_dir, d + '.dcm')
+                    shutil.copy(f_path, f_path_new)
+                    print(f'Copied {f_path} to {f_path_new}')
+                elif f.endswith('.dcm.tag'):
+                    elems = os.path.splitext(os.path.splitext(f)[0])
+                    f_name = elems[0]
+                    f_path = os.path.join(d_path, f)
+                    f_path_new = os.path.join(data_dir, d + '.dcm.tag')
+                    shutil.copy(f_path, f_path_new)
+                    print(f'Copied {f_path} to {f_path_new}')
+                else:
+                    pass
